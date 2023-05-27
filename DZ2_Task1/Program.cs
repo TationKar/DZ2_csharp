c58b5ccc6cae701c2b40b11b84527d98e465accd
Timur Karimov <tationkar@gmail.com>
2023-05-27 16:48:40 +0300

    first task DZ2

---
+++ DZ2_Task1/Program.cs
@@ -0,0 +1,11 @@
+﻿/* Задача 10: Напишите программу, которая принимает 
+на вход  трёхзначное число и на выходе показывает 
+вторую цифру этого числа.
+456 -> 5
+782 -> 8
+918 -> 1 */
+Console.Clear();
+Console.Write("Введите целое трехзначное число: ");
+int threeDigit = int.Parse(Console.ReadLine()!);
+int twoDigit = threeDigit % 100;
+Console.WriteLine($"Tрехзначное число {threeDigit} - вторая цифра {twoDigit / 10}");
