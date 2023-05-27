f32288b52ff2a429a10d33350d7524507608fe69
Timur Karimov <tationkar@gmail.com>
2023-05-27 18:19:51 +0300

    second task DZ2

---
+++ DZ2_Task2/Program.cs
@@ -0,0 +1,21 @@
+﻿/* Задача 13: Напишите программу, которая выводит 
+третью цифру заданного числа или сообщает, что третьей цифры нет.
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6 */
+Console.Clear();
+Console.Write("Введите целое число: ");
+int num = int.Parse(Console.ReadLine()!);
+if (num < 100)
+    Console.WriteLine("третьей цифры нет");
+else if (num < 1000 && num >100)
+    Console.WriteLine($"Tретья цифра в числе {num} - {num % 10}");
+int intermdt = num;
+if (num > 999)
+{
+    while (num / 10 > 99)
+    {
+        num = num / 10;
+    }
+    Console.WriteLine($"Tретья цифра в числе {intermdt} - {num % 10}");
+}
