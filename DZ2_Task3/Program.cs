3416158b9494fd7ebf67107b1de4a6e72d259516
Timur Karimov <tationkar@gmail.com>
2023-05-27 18:42:21 +0300

    third task DZ2

---
+++ DZ2_Task3/Program.cs
@@ -0,0 +1,12 @@
+﻿/* Задача 15: Напишите программу, которая принимает на вход цифру, 
+обозначающую день недели, и проверяет, является ли этот день выходным.
+6 -> да
+7 -> да
+1 -> нет */
+Console.Clear();
+int dayNum = new Random().Next(1, 7);
+
+if (dayNum == 6 || dayNum == 7)
+    Console.WriteLine($"{dayNum} -> да");
+else
+    Console.WriteLine($"{dayNum} -> нет");
